7cb3a4bd1003144ac5318ca13bdbb0c00299f760
girilv <girilv@gmail.com>
2021-09-07 08:09:27 +0530

    added for users

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var placeRouter = require('./routes/place')
+var placeRouter = require('./routes/place');
 
 var app = express();
 
