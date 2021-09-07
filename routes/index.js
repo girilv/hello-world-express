1c4588259eef08c14a733d7fefbc60069251ae85
girilv <girilv@gmail.com>
2021-09-07 07:43:25 +0530

    with azure pipeline

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'This is a node app hello from Azure pipeline' });
+  res.render('index', {body: "From giri"});
 });
 
 module.exports = router;
